63e68dc3e2205359231bbbb44ed088ad3985a0ce
Juan Fernando Castro M. <jfcastro9208@gmail.com>
2024-03-06 14:42:52 -0500

    :tada: Initial config nestJS - Dynamo - AWS Lambda with Graphql

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { DynamooseModule } from 'nestjs-dynamoose';
+
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+
+import { NotificationModule } from './modules/notification/notification.module';
+import { UserModule } from './modules/user/user.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+    }),
+    DynamooseModule.forRoot({
+      local: process.env.IS_DDB_LOCAL === 'true',
+      aws: { region: process.env.REGION },
+      table: {
+        create: process.env.IS_DDB_LOCAL === 'true',
+        prefix: `${process.env.SERVICE}-${process.env.STAGE}-`,
+        suffix: '-table',
+      },
+    }),
+    NotificationModule,
+    UserModule
+  ],
+})
+export class AppModule {}
